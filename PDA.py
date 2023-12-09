65b9aed70e36c30e8caa93827ec656e85bdaa62a
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2023-12-10 01:35:26 +0800

    try again

---
+++ PDA.py
@@ -0,0 +1,41 @@
+from typing import List, Self
+from collections import deque
+from State import State
+
+class PDA:
+    def __init__(self) -> None:
+        self.q: List[State] = list()
+        self.gamma: deque[str] = deque()
+        self.gamma.append("Z")
+
+    def delta(self, input: str) -> bool:
+        isAccepted: bool = False
+        currentState: State = self.q[0]
+        currentState.showStack()
+
+                    
+        return isAccepted
+    
+    def main(self) -> None:
+        q0: State = State(self.gamma)
+        q1: State = State(self.gamma)
+        # q2: State = State(self.gamma)
+        # q2.setFinal()
+        q0.setTransition("1", q1, "1")
+        q0.setTransition("0", q0, "0")
+        # q1.setTransition("0", q1, "0")
+        self.q.append(q0)
+        self.q.append(q1)
+        # self.q.append(q2)
+
+        # q0: State = State(self.gamma)
+        # q1: State = State(self.gamma)
+
+
+        isAccepted: bool = self.delta("010")
+
+
+
+if __name__ == "__main__":
+    pda: PDA = PDA()
+    pda.main()
--- State.py
+++ State.py
@@ -1,22 +1,35 @@
+from types import NoneType
 from typing import List, Self
 from collections import deque
 
 class State:
     def __init__(self, stack: deque[str]):
         self.f: bool =  False
-        self.gamma: deque = stack
+        self.gamma: deque[str] = stack
         self.next: List[List[State]] = []
 
-    def delta(self, symbol: str) -> List[str]:
+    def delta(self, symbol: str) -> List[Self]:
         '''Returns next states given input symbol.'''
-        output = list()
-
-
-        return output
+        
+        return self.next[int(symbol)]
     
-    def setTransition(self, input_symbol: str, pop_symbol:str, next_state: Self, push_symbol: str):
-        self.next.append([self, next_state])
-        pass
+    def setTransition(self, input_symbol: str, next_state: Self, push_symbol: str, pop_symbol:str|None = None):
+        '''Sets transition to the next state'''
+
+        self.gamma.append(push_symbol)
+        # self.next[int(input_symbol)] = [next_state]
+        # if pop_symbol != None:
+        #     self.gamma.pop()
+        # if push_symbol[-1] == self.getStackSymbol() and len(push_symbol) > 1:
+        #     self.gamma.append(push_symbol[-1])
+        # if pop_symbol != None:
+        #     next_state.gamma.pop()
+        
+        # if push_symbol != None and len(push_symbol) > 1:
+        #     for letter in push_symbol:
+        #         next_state.gamma.append(letter)
+
+        # self.next.insert(int(input_symbol), [next_state])
 
     def setFinal(self) -> None:
         '''Sets self.f to True if state is a final state'''
@@ -27,7 +40,8 @@ class State:
         return self.gamma[-1]
     
     def showStack(self) -> None:
-        print(self.gamma)
+        for symbol in reversed(self.gamma):
+            print(f'|{symbol}|\n')
 
 
 
