bf6103243daed1b7efff2f9c89a456033ef34deb
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2023-12-20 18:33:52 +0800

    added comments

--- PDA.py
+++ PDA.py
@@ -5,25 +5,41 @@ from Transition import Transition
 
 class PDA:
     def __init__(self) -> None:
-        self.q: List[State] = list()
         self.gamma: deque[str] = deque()
         self.transitions: List[Transition] = list()
         self.gamma.append("Z")
 
     def delta(self, string_input: str, initial_state: State) -> bool:
+        """Accepts or rejects a string based on the transition functions and prints transitions and stack at each step
+
+        Args:
+            string_input (str): string to be accepted or rejected
+            initial_state (State): initial state of the PDA
+
+        Returns:
+            bool: True if string is accepted, False otherwise
+        """        
 
         currentState: State = initial_state
         string_input += 'e'
 
+
+        '''
+        Loops through the string and performs the transition function
+        Breaks and rejects string if no transition can be done based on current top of stack symbol and input symbol and current state 
+        '''
         for symbol in string_input:
             print("---\n")
             currentState.showStack()
-            transition :Transition|None = self.search_in_transitions(currentState, symbol, currentState.getStackSymbol())
+            transition :Transition|None = self.search_in_transitions(currentState, symbol, currentState.getTopStackSymbol())
 
+            # No transition found
             if transition == None:
-                    return False
+                return False
             
-            print(f'Transition function\nd({currentState.rep}, {symbol}, {currentState.getStackSymbol()}) = ({transition.next_state.rep}, {transition.push_symbol})')
+            print(f'Transition function\nd({currentState.rep}, {symbol}, {currentState.getTopStackSymbol()}) = ({transition.next_state.rep}, {transition.push_symbol})')
+            
+            # Sets the stack of next state
             next_stack: deque[str] = self.perform_stack_operations(currentState.gamma, transition.push_symbol)
             currentState = transition.next_state
             currentState.setStack(next_stack)
@@ -31,6 +47,15 @@ class PDA:
         return True
 
     def perform_stack_operations(self, stack: deque[str], push_symbol: str) -> deque[str]:
+        """Performs stack operations based on the push symbol
+
+        Args:
+            stack (deque[str]): stack of the state
+            push_symbol (str): symbols to be pushed to the stack
+
+        Returns:
+            deque[str]: stack after performing the operations
+        """        
         stack.pop()
         if push_symbol == "e":
             return stack
@@ -41,6 +66,17 @@ class PDA:
         return stack
     
     def search_in_transitions(self, current_state: State, input_symbol: str, stack_top_symbol: str) -> Transition|None:
+        """Searches for a transition in the list of transitions
+
+        Args:
+            current_state (State): current state
+            input_symbol (str): current input symbol
+            stack_top_symbol (str): current top of stack symbol
+
+        Returns:
+            Transition|None: Transition object if found, None otherwise
+        """        
+        
         for transition in self.transitions:
             if transition.current_state == current_state and transition.input_symbol == input_symbol and transition.pop_symbol == stack_top_symbol:
                 return transition
@@ -52,7 +88,7 @@ class PDA:
         q2: State = State(self.gamma, "q2")
         q2.setFinal()
 
-        self.transitions.append(Transition(q0, "0", "Z", q0, "0Z"))
+        self.transitions.append(Transition(q0, "0", "Z",  q0, "0Z"))
         self.transitions.append(Transition(q0, "0", "0",  q0, "00"))
         self.transitions.append(Transition(q0, "1", "0",  q1, "e"))
         self.transitions.append(Transition(q1, "1", "0",  q1, "e"))
--- State.py
+++ State.py
@@ -4,24 +4,36 @@ from collections import deque
 
 class State:
     def __init__(self, stack: deque[str], rep: str):
-        self.f: bool =  False
-        self.gamma: deque[str] = stack
-        self.rep = rep
+        self.f: bool =  False               # Treue fi state is final state
+        self.gamma: deque[str] = stack      # Represents the stack
+        self.rep: str = rep                      # String representation of the name of the state
 
     def setFinal(self) -> None:
-        '''Sets self.f to True if state is a final state'''
+        """Sets state to final state'''
+        """      
         self.f = True
 
-    def getStackSymbol(self) -> str:
-        '''Returns top of the stack'''
+    def getTopStackSymbol(self) -> str:
+        """Gets the symbol on the top of the stack
+
+        Returns:
+            str: symbol on top of the stack
+        """        
         return self.gamma[-1]
     
     def showStack(self) -> None:
+        """Prints the stack vertically
+        """        
         for symbol in reversed(self.gamma):
             print(f'|{symbol}|\n')
     
 
     def setStack(self, stack:deque[str]) -> None:
+        """Sets the stack of the state
+
+        Args:
+            stack (deque[str]): Deque object containing strings that represents the stack
+        """        
         self.gamma = stack
 
 
