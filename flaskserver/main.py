03cb811f52dc6e09555af51c2115b91517ff70ca
Jeremy Jobert Peñafiel <100175951+jeremypenafiel@users.noreply.github.com>
2024-06-19 21:57:24 +0800

    changed to my_app

--- flaskserver/main.py
+++ flaskserver/main.py
@@ -1,11 +1,11 @@
 from waitress import serve
 
-import server 
+from server import my_app
 
 PORT = 8000
 mode = "prod"
 if __name__ == '__main__':
         if mode == "dev":
-            server.my_app.run( port=PORT, debug=True)
+            my_app.run( port=PORT, debug=True)
         else:
-                serve(server.my_app,port=PORT, threads=1)
+                serve(my_app,port=PORT, threads=1)
